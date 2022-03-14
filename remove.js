5b5d2c31beee6fcb1000cbfefcda3ab89c60eaad
Melissa MEBARKI <78750936+melissa966@users.noreply.github.com>
2022-03-14 18:55:33 +0100

    Delete remove.js

--- remove.js
+++
@@ -1,9 +0,0 @@
-//Melissa MEBARKI
-var array = [0, 0, 1, 1, 1, 2, 2, 3, 4, 4, 5, 5, 5, 5, 6, 7, 7, 8];
-function remove(array, val) {
-  return array.filter(function (i) {
-    return i != val;
-  });
-}
-
-console.log(remove(array, 7));
